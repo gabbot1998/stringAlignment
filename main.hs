c2d135cc56c88439ec87e6e68cd901a416adf2d4
mahirhambiralovic <mahir.hambiralovic@gmail.com>
2020-05-04 16:04:47 +0200

    major breakthroughs

--- main.hs
+++ main.hs
@@ -1,10 +1,11 @@
+import Debug.Trace
+
+main = do print (sim "hej" "hej")
 
 scoreMatch = 1
 scoreMisMatch = -1
 scoreSpace = -2
 minimumInteger = -99999
-
-type ALignmentType = (String, String)
 
 similarityScore :: String -> String -> Int
 similarityScore _ [] = 0
@@ -17,20 +18,44 @@ similarityScore (s1:ss1) (s2:ss2)
 attachHeads :: a -> a -> [([a],[a])] -> [([a],[a])]
 attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
 
-maximaBy :: (a -> Int) -> [a] -> [a]
-maximaBy valueFcn xs = maximaByHelp valueFcn xs [] minimumInteger
+maximaBy :: (Integral b) => (a -> b) -> [a] -> [a]
+maximaBy valueFcn xs = maximaByHelp valueFcn xs 0 -- Calls with largest n = 0
 
---Varför fungerar inte typparametrarna som vi tror att de ska göra! :O
-maximaByHelp :: (a -> Int) -> [a] -> [a] -> Int -> [a]
-maximaByHelp _ [] ys _ = ys
-maximaByHelp valueFcn (x:xs) ys max_n
-  | val < max_n = maximaByHelp valueFcn xs ys max_n
-  | val > max_n = maximaByHelp valueFcn xs [x] val
-  | otherwise = maximaByHelp valueFcn xs (x:ys) val
+maximaByHelp :: (Ord b) => (a -> b) -> [a] -> b -> [a]
+maximaByHelp _ [] _ = []
+maximaByHelp valueFcn (x:xs) max_n
+  | val < max_n = maximaByHelp valueFcn xs max_n
+  | otherwise = x : maximaByHelp valueFcn xs val
   where val = valueFcn x
 
+type AlignmentType = (String,String)
 -- optAlignments :: String -> String -> [AlignmentType]
--- optAlignments string1 string2 =
 
-lieToMe = show("Gabbe made this code alone")
+sim :: String -> String -> Int
+sim [] [] = 0
+sim (x:xs) [] = sim xs [] + score [x] "-"
+sim [] (y:ys) = sim [] ys + score [y] "-"
+sim (x:xs) (y:ys) = max (max (sim xs ys + score [x] [y]) (sim xs (y:ys) + score [x] "-"))
+                          (sim (x:xs) ys + score "-" [y] )
+score = similarityScore
+
+mcsLength :: String -> String -> Int
+mcsLength xs ys = mcsLen (length xs) (length ys)
+  where
+    mcsLen i j = mcsTable!!i!!j
+    mcsTable = [[ mcsEntry i j | j<-[0..]] | i<-[0..] ]
+
+    mcsEntry :: Int -> Int -> Int
+    mcsEntry _ 0 = 0
+    mcsEntry 0 _ = 0
+    mcsEntry i j
+      | x == y    = 1 + mcsLen (i-1) (j-1)
+      | otherwise = max (mcsLen i (j-1))
+                        (mcsLen (i-1) j)
+      where
+         x = xs!!(i-1)
+         y = ys!!(j-1)
+
+
+lieToMe = show("Gabbe was involved in writing this code")
 truth = show("Mahir made this code alone")
